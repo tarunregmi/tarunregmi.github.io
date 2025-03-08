30da9cdd9658bf2c27a7153f5ddebaab0d589748
Tarun Regmi <tarunregmi.io@gmail.com>
2025-03-08 22:32:01 +0545

    update: change to support 404 page

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,19 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte()],
-	server: {
-		port: 2012,
-		host: true,
-	}
+  plugins: [svelte()],
+  server: {
+    port: 2012,
+    host: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith(".css")) return "assets/index.css";
+          return assetInfo.name;
+        },
+      },
+    },
+  },
 });
