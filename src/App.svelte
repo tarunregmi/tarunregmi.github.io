8cf505c30533874c44b18475c4f6b41b340bccd0
Tarun Regmi <tarunregmi.io@gmail.com>
2025-03-13 12:34:27 +0545

    remove dev message

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,8 @@
 	import Router from "svelte-spa-router";
 	import routes from "./routes";
 	import Header from "./components/Header.svelte";
-	import DevMsg from "./components/DevMsg.svelte";
 </script>
 
-<DevMsg />
 <Header />
 <main class="width-full box">
 	<Router {routes} />
